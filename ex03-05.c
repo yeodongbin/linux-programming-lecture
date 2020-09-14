043685a4cba10af812cf88ec141765a153de1e84
Yeo, Dongbin <yeodongbin2011@gmail.com>
2020-09-14 16:56:18 +0900

    Update ex03-05.c

--- ex03-05.c
+++ ex03-05.c
@@ -1,8 +1,7 @@
-
-
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 int main()
 {
@@ -14,5 +13,5 @@ int main()
     /* 읽기/쓰기 포인터를 EOF로 이동한다. */
     newpos = lseek(filedes, (off_t)0, SEEK_END);
 
-    printf("file size : %d\n", newpos);
+    printf("file size : %ld\n", newpos);
 }
